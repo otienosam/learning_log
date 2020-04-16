b8444e39a21d8df56a5540e47144a43e3a7a9619
maestro <otienosam.odhiambp@gmail.com>
2020-04-16 22:32:06 +0300

    defining models

---
+++ learning_logs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """A topic the user is learning about."""
+    text = models.CharField(max_length=200)#data that’s made up of characters,
+    # or text
+    date_added = models.DateTimeField(auto_now_add=True)#automatically set
+    #to the current date and time whenever the user creates a new topic.
+
+    def __str__(self):#diplays simple representation of a model
+        """Return a string representation of the model."""
+        return self.text
