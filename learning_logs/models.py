84524727d06250ad5de102c601269d4bf2faf944
maestro <otienosam.odhiambp@gmail.com>
2020-04-16 23:41:30 +0300

    Defining the Entry Model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -16,3 +16,22 @@ class Topic(models.Model):
      # modify models.py,
      # call makemigrations on learning_logs ,
      #and tell Django to migrate the project.
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    #connects each entry to a specific topic
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)#foreign key instance
+    #The on_delete=models.CASCADE argument tells Django that when a topic
+    #is deleted, all the entries associated with that topic should be
+    #deleted as well
+    text = models.TextField()#instanceof TextField
+    date_added = models.DateTimeField(auto_now_add=True)#allows presentation of
+    #entries in the order they were created and to place a timestamp next to
+    #each entry.
+
+    class Meta:#nested class to hold extra information for managing a model
+    verbose_name_plural = 'entries'#used when there is need to refer to more than
+    #one entry by django
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
