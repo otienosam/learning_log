9785433699f8f46d6e40d76e4c22d0b92ac805e0
maestro <otienosam.odhiambp@gmail.com>
2020-04-16 23:07:21 +0300

    Registering a Model with the Admin Site

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Topic#The dot in front of models tells Django to look for
+# models.py in the same directory as admin.py
+
+admin.site.register(Topic)# mean manage our model through the admin site
