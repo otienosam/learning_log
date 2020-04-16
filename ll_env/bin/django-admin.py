84f7a96607a6274dcb59f608c2859277c89f1a2b
maestro <otienosam.odhiambp@gmail.com>
2020-04-16 16:12:02 +0300

    Django project created,sqlite database created

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sammy/Python/projects/web_applications/Django_start/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
