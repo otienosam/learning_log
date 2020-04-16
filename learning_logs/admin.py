3cbe8a661881c3e42d36a53141745cdc8a285abb
maestro <otienosam.odhiambp@gmail.com>
2020-04-16 23:56:59 +0300

    Registering Entry with the Admin Site

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -5,3 +5,4 @@ from .models import Topic#The dot in front of models tells Django to look for
 # models.py in the same directory as admin.py
 
 admin.site.register(Topic)# mean manage our model through the admin site
+admin.site.register(Entry)
