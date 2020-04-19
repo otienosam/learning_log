6f878507eaae9c3307b8f61ac0facab1fdcb24db
maestro <otienosam.odhiambp@gmail.com>
2020-04-19 23:25:05 +0300

    Set DEBUG based on environment variables.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -134,3 +134,8 @@ LOGIN_URL = 'users:login'#when an unauthenticated user requests a page
 # Heroku settings.
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+DEBUG = False
